7e0872ae876f89735c291ee3556262c0ea759f4c
Danilo <danilosaldanha68@gmail.com>
2026-05-28 21:16:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+
+    themeToggle.innerHTML = "☀️";
+
+  } else {
+
+    themeToggle.innerHTML = "🌑";
+
+  }
+
+});
